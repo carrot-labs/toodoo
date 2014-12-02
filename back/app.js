93469e1c22631798db7b865dbe78c1f9a512729b
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2014-12-02 16:30:28 -0200

    Create app.js

---
+++ back/app.js
@@ -0,0 +1,47 @@
+/*
+ * Require the needed modules
+ */
+var express        = require('express');
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
+var errorHandler   = require('errorhandler');
+var morgan         = require('morgan');
+var http           = require('http');
+var path           = require('path');
+
+// Instantiate the express app
+var app = module.exports = express();
+
+
+/**
+ * Configure defaults
+ */
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/modules');
+app.set('view engine', 'jade')/
+
+
+/**
+ * Configure middlewares
+ */
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(methodOverride());
+app.use(express.static(path.join(__dirname, '../front')));
+
+
+/**
+ * Configure development environment
+ */
+var env = process.env.NODE_ENV || 'development';
+
+if(env === 'development') {
+	app.use(errorHandler());
+}
+
+
+/**
+ * Export the app
+ */
+module.exports = app;
