07fc61071221c7948b98581ea7f3120cf6a55fad
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2014-12-03 09:56:13 -0200

    Create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+/**
+ * Require gulp
+ */
+var gulp = require('gulp');
+
+
+/**
+ * Require the needed modules
+ */
+var clean  = require('gulp-clean');
+var concat = require('gulp-concat');
+var jade   = require('gulp-jade');
+var jshint = require('gulp-jshint');
+var rename = require('gulp-rename');
+var stylus = require('gulp-stylus');
+var uglify = require('gulp-uglify');
+var watch  = require('gulp-watch');
+
+
+/**
+ * Javascript files
+ */
+var jsFiles = ['back/**/*.js'];
+
+
+/**
+ * Lint task
+ */
+gulp.task('lint', function() {
+	return gulp.src(jsFiles)
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'));
+});
+
+
+/**
+ * Watch task
+ */
+gulp.task('watch', function() {
+	gulp.watch(jsFiles, ['lint']);
+});
+
+
+/**
+ * Default task
+ */
+gulp.task('default', ['lint']);
