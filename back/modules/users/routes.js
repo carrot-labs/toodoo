4b075129af45b855a3faac7c67c35d9330d51219
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2014-12-03 10:40:02 -0200

    Create users routes

---
+++ back/modules/users/routes.js
@@ -0,0 +1,78 @@
+/**
+ * Require the needed modules
+ */
+var express = require('express');
+
+
+/**
+ * Create the express router
+ */
+var router = express.Router();
+
+
+/**
+ * Require the controller
+ */
+var _user = require('./controller');
+
+
+/**
+ * Define the default callback
+ */
+var cb = function(err, data, res) {
+	if(err) throw err;
+
+	res.json(data);
+};
+
+
+/**
+ * Create the GET routes
+ */
+
+router.get('/', function(req, res) {
+	_user.retrieve(req, res, cb);
+});
+
+router.get('/_id/:id', function(req, res) {
+	_user.findOneBy_Id(req, res, cb);
+});
+
+router.get('/id/:id', function(req, res) {
+	_user.findOneById(req, res, cb);
+});
+
+router.get('/username/:username', function(req, res) {
+	_user.findOneByUsername(req, res, cb);
+});
+
+
+/**
+ * Create the POST routes
+ */
+router.post('/', function(req, res) {
+	_user.create(req, res, cb);
+});
+
+
+
+/**
+ * Create the PUT routes
+ */
+router.put('/_id/:id', function(req, res) {
+	_user.update(req, res, cb);
+});
+
+
+
+/**
+ * Create the DELETE routes
+ */
+router.delete('/_id/:id', function(req, res) {
+	_user.delete(req, res, cb);
+});
+
+
+/**
+ * Export the router;
+ */
