b3c576ea8e9ab0646dde4fc533c85f615c4e6d43
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2014-12-03 10:37:44 -0200

    Create todos routes

---
+++ back/modules/todos/routes.js
@@ -0,0 +1,75 @@
+/**
+ * Require the needed modules
+ */
+var express = require('express');
+
+
+/**
+ * Create the express router
+ */
+var router = express.Router();
+
+
+/**
+ * Require the controller
+ */
+var _todo = require('./controller');
+
+
+/**
+ * Define the default callback
+ */
+var cb = function(err, data, res) {
+	if(err) throw err;
+
+	res.json(data);
+};
+
+
+/**
+ * Create the GET routes
+ */
+
+router.get('/', function(req, res) {
+	_todo.retrieve(req, res, cb);
+});
+
+router.get('/_id/:id', function(req, res) {
+	_todo.findOneBy_Id(req, res, cb);
+});
+
+router.get('/id/:id', function(req, res) {
+	_todo.findOneById(req, res, cb);
+});
+
+
+
+/**
+ * Create the POST routes
+ */
+router.post('/', function(req, res) {
+	_todo.create(req, res, cb);
+});
+
+
+
+/**
+ * Create the PUT routes
+ */
+router.put('/_id/:id', function(req, res) {
+	_todo.update(req, res, cb);
+});
+
+
+
+/**
+ * Create the DELETE routes
+ */
+router.delete('/_id/:id', function(req, res) {
+	_todo.delete(req, res, cb);
+});
+
+
+/**
+ * Export the router;
+ */
