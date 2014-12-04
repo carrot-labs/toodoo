2922b49ec61cc10f817322420c16d53c5d850d2c
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2014-12-04 21:09:59 -0200

    Add gulp-plumber dependency to handle gulp errors

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,15 @@ var gulp = require('gulp');
 /**
  * Require the needed modules
  */
-var clean  = require('gulp-clean');
-var concat = require('gulp-concat');
-var jade   = require('gulp-jade');
-var jshint = require('gulp-jshint');
-var rename = require('gulp-rename');
-var stylus = require('gulp-stylus');
-var uglify = require('gulp-uglify');
-var watch  = require('gulp-watch');
+var clean   = require('gulp-clean');
+var concat  = require('gulp-concat');
+var jade    = require('gulp-jade');
+var jshint  = require('gulp-jshint');
+var plumber = require('gulp-plumber')
+var rename  = require('gulp-rename');
+var stylus  = require('gulp-stylus');
+var uglify  = require('gulp-uglify');
+var watch   = require('gulp-watch');
 
 
 /**
@@ -31,10 +32,11 @@ var jadeDest  = 'public/';
  */
 gulp.task('scripts', function() {
 	return gulp.src(jsFiles)	
-			.pipe(jshint())
-			.pipe(jshint.reporter('default'))
-			.pipe(concat('app.js'))
-			// .pipe(uglify())
+		.pipe(plumber())
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'))
+		.pipe(concat('app.js'))
+		.pipe(uglify())
 		.pipe(gulp.dest(jsDest));
 });
 
@@ -45,7 +47,8 @@ gulp.task('scripts', function() {
 
 gulp.task('views', function() {
 	return gulp.src(jadeFiles)
-			.pipe(jade())
+		.pipe(plumber())
+		.pipe(jade())
 		.pipe(gulp.dest(jadeDest));
 });
 
