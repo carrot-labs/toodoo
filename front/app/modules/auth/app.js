03f64d1a2ad1cc29a3799c00f8692f6bd4dc1095
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2014-12-10 22:50:36 -0200

    Create auth app.js

---
+++ front/app/modules/auth/app.js
@@ -0,0 +1,19 @@
+(function(angular) {
+
+	'use strict';
+
+	/**
+	 * Create the controllers module
+	 */
+	angular
+		.module('toodoo.modules.Auth.controllers', []);
+
+
+	/**
+	 * Create the services module
+	 */
+	angular
+		.module('toodoo.modules.Auth.services', []);
+
+	
+})(angular);
