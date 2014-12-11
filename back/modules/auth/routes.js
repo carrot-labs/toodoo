ddc178f26bff71e601d5352460a7fa99f3ad60e4
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2014-12-10 23:30:01 -0200

    Add login route to auth module

--- back/modules/auth/routes.js
+++ back/modules/auth/routes.js
@@ -2,6 +2,7 @@
  * Require the needed modules
  */
 var express = require('express');
+var jwt     = require('jsonwebtoken');
 
 
 /**
@@ -31,7 +32,41 @@ var cb = function(err, data, res) {
  */
 
 router.post('/login', function(req, res) {
-	res.json('hello there');
+	var username = req.body.username;
+	var password = req.body.password;
+
+	var secret = 'secret';
+
+
+	/**
+	 * Verify if the username and password are incorrect
+	 */
+	if(username !== 'johndoe' || password !== 'foobar') {
+		res.send(401, 'Wrong user or password');
+	}
+
+
+	/**
+	 * Get the user information
+	 */
+	var profile = {
+		name: 'John Doe',
+		age: 34,
+		job: 'Web developer',
+		email: 'johndoe@mail.com'
+	};
+
+
+	/**
+	 * Generate the token
+	 */
+	var token = jwt.sign(profile, secret, { expiresInMinutes: 60*24*7 });
+
+
+	/**
+	 * Send the token as json
+	 */
+	res.json({ token: token});
 });
 
 
